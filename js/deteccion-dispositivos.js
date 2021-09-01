a85e222ab0d550f829dde92171402cdf412d349d
Aldo Navarrete <aaldiitoo@gmail.com>
2021-09-01 14:00:22 -0500

    Iniciando UserAgent

---
+++ js/deteccion-dispositivos.js
@@ -0,0 +1,10 @@
+const d = document,n = navigator, ua = n.userAgent;
+export default function userDeviceInfo(id){
+    const $id       = d.getElementById(id), 
+          isMobile  = {},
+          isDesktop = {},
+          isBrowser = {};
+
+    
+    
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -6,6 +6,7 @@ import scrollTopButton from './btn_scroll.js';
 import darkTheme from './tema-oscuro.js';
 import responsiveMedia from './objeto-responsive.js';
 import responsiveTester from './prueba_responsive.js';
+import userDeviceInfo from './deteccion-dispositivos.js';
 const d = document;
 d.addEventListener("DOMContentLoaded",e=>{
     hamburguerMenu(".panel-btn",".panel",".menu a");
@@ -22,7 +23,9 @@ d.addEventListener("DOMContentLoaded",e=>{
     `<a target="_blank" rel="noopener noreferrer" href="https://goo.gl/maps/eCswiW4o4yn7wXw36" >Ver Mapa</a>`,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.3308153496612!2d-99.16869369179001!3d19.427020596721842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1629818531580!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`);
 
-    responsiveTester("responsive-tester")
+    responsiveTester("responsive-tester");
+    userDeviceInfo("user-device")
+
 })
 
 //La propiedad keydown tiene un code 18, viene
