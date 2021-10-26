51f5bd147b4cb8946dc1fe4e0d6a6bd81c229b6e
aldonavarretefp <aaldiitoo@gmail.com>
2021-10-26 08:07:08 -0500

    User Agent para device-detection

--- js/deteccion-dispositivos.js
+++ js/deteccion-dispositivos.js
@@ -1,10 +1,57 @@
 const d = document,n = navigator, ua = n.userAgent;
 export default function userDeviceInfo(id){
-    const $id       = d.getElementById(id), 
-          isMobile  = {},
-          isDesktop = {},
-          isBrowser = {};
 
+    const $id       = d.getElementById(id), 
+          isMobile  = {
+              android: () => ua.match(/Android/i),
+              ios: () => ua.match(/iPhone|iPad|iPod/i),
+              windows: () => ua.match(/Windows Phone/i),
+              any: function(){
+                  return this.android() || this.ios() || this.windows();
+              }
+          },
+          isDesktop = {
+                mac: () => ua.match(/Mac/i),
+                windows: () => ua.match(/Windows/i),
+                linux: () => ua.match(/Linux/i),
+                any: function(){
+                    return this.mac() || this.windows() || this.linux();
+                }
+          },
+          isBrowser = {
+                chrome: () => ua.match(/Chrome/i),
+                firefox: () => ua.match(/Firefox/i),
+                safari: () => ua.match(/Safari/i),
+                opera: () => ua.match(/Opera/i),
+                ie: () => ua.match(/MSIE/i),
+                any: function(){
+                    return this.chrome() || this.firefox() || this.safari() || this.opera() || this.ie();
+                }
+            };
+            console.log($id);
+    if(isMobile.any()){
+        $id.innerText += 'Mobile';
+    }
+    if(isDesktop.any()){
+        $id.innerText += ' Desktop';
+    }
+    if(isBrowser.any()){
+        $id.innerText += ' Browser';
+    }
+    console.log(isMobile.any());
+    $id.innerHTML += `
+        <ul>
+            <li><b>User Agent:</b> ${ua}</li>
+            <li><b>Plafaforma:</b> ${isMobile.any() ? isMobile.any() : isDesktop.any()}</li>
+            <li><b>Browser:</b> ${isBrowser.any() ? isBrowser.any() : 'Not detected'}</li>
+    `;
+
+    if(isBrowser.chrome()){
+        $id.innerHTML += `
+            <p>Chrome: ${ua.match(/Chrome\/(\d+)/i)[1]}
+            Contenido que solo se puede ver en Chrome</p>
+        `;
+    }
     
     
 }
--- js/index_dom.js
+++ js/index_dom.js
@@ -24,7 +24,7 @@ d.addEventListener("DOMContentLoaded",e=>{
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.3308153496612!2d-99.16869369179001!3d19.427020596721842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1629818531580!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`);
 
     responsiveTester("responsive-tester");
-    userDeviceInfo("user-device")
+    userDeviceInfo("user-device");
 
 })
 
